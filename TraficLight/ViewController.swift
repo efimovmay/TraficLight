946f4dc89f25cce7e8cbbd6f71be8fbcfbfd4df0
Aleksey Efimov <smaylik121@gmail.com>
2022-11-29 13:27:03 +0300

    Added logic

--- TraficLight/ViewController.swift
+++ TraficLight/ViewController.swift
@@ -8,18 +8,55 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private enum CurrentLight{
+        case red, orange, green
+    }
+    private var currentLight = CurrentLight.green
+    
     @IBOutlet var redLight: UIView!
     @IBOutlet var orangeLight: UIView!
     @IBOutlet var greenLight: UIView!
     
-    @IBOutlet var actionBotton: UIButton!
+    @IBOutlet var actionButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        actionButton.setTitle("START", for: .normal)
+        
     }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        redLight.layer.cornerRadius = redLight.bounds.size.width * 0.5
+        orangeLight.layer.cornerRadius = orangeLight.bounds.size.width * 0.5
+        greenLight.layer.cornerRadius = greenLight.bounds.size.width * 0.5
+    }
+    
+    
+    @IBAction func changeLight() {
 
-
+        if actionButton.currentTitle == "START" {
+            actionButton.setTitle("Next", for: .normal)
+        }
+        
+        switch currentLight {
+        case .red:
+            redLight.alpha = 0.3
+            orangeLight.alpha = 1
+            currentLight.self = .orange
+        case .orange:
+            orangeLight.alpha = 0.3
+            greenLight.alpha = 1
+            currentLight.self = .green
+        case .green:
+            greenLight.alpha = 0.3
+            redLight.alpha = 1
+            currentLight.self = .red
+        }
+        
+    }
+    
 }
-
