21339ad61c883515b6a4da95f3509730caf0efba
Aleksey Efimov <smaylik121@gmail.com>
2022-11-29 12:44:02 +0300

    Add view and botton

--- TraficLight/ViewController.swift
+++ TraficLight/ViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var orangeLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    
+    @IBOutlet var actionBotton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
